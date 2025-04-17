1fddc046ad2698584dd45ebde9d316a6b1266b73
Sasha Niiakyi <etrigan070@gmail.com>
2025-04-17 18:13:46 +0300

    change local executor, using pty instead subprocess

--- executor/local_executor.py
+++ executor/local_executor.py
@@ -1,15 +1,22 @@
-import subprocess
+import os
+import pty
 
 from .base_executor import BaseExecutor
 
 
 class LocalExecutor(BaseExecutor):
 	def execute(self, command: str):
-		try:
-			result = subprocess.run(
-				command, shell=True, check=True,
-				stdout=subprocess.PIPE, stderr=subprocess.PIPE
-			)
-			return result.stdout.decode() + result.stderr.decode()
-		except subprocess.CalledProcessError as e:
-			return e.stdout.decode() + e.stderr.decode()
+		pid, fd = pty.fork()
+		if pid == 0:
+			os.execvp("bash", ["bash", "-c", command])
+		else:
+			output = b""
+			while True:
+				try:
+					data = os.read(fd, 1024)
+					if not data:
+						break
+					output += data
+				except OSError:
+					break
+			return output.decode()
